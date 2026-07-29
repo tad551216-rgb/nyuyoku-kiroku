43459e7070ef4002981d132eee16fb8625883822
tad551216-rgb <tad551216@gmail.com>
2026-07-29 14:26:03 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'nyuyoku-kiroku-v11';
+const CACHE = 'nyuyoku-kiroku-v12';
 const ASSETS = ['./','./index.html','./manifest.json','./qrcode.js','./jsQR.js','./icon-192.png','./icon-512.png','./icon-180.png'];
 
 /* 入れておく。1つ失敗しても、ほかは入れる */
